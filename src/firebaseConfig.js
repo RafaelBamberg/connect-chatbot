cc105576b5fafd0bc1ee1df543bf50bbbff4c1ba
RafaelBamberg <raafaelbamberg@gmail.com>
2025-06-22 09:56:56 -0300

    commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,14 @@ const { getAuth } = require("firebase/auth");
 
 // Firebase configuration - load from .env file for security
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
+  databaseURL: `https://${process.env.FIREBASE_PROJECTID}.firebaseio.com`,
 };
 
 // Initialize Firebase
